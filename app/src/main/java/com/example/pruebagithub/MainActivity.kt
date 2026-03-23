ebce455f054c74345e83578930e7ae676d763f50
Daniel López Escuder <d.lopezescuder@edu.gva.es>
2026-03-23 13:18:01 +0100

    Segundo Commit

--- app/src/main/java/com/example/pruebagithub/MainActivity.kt
+++ app/src/main/java/com/example/pruebagithub/MainActivity.kt
@@ -5,13 +5,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.pruebagithub.ui.theme.PruebaGitHubTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,28 +30,33 @@ class MainActivity : ComponentActivity() {
         setContent {
             PruebaGitHubTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                   Box(Modifier.padding(innerPadding)){
+                       MainScreen()
+                   }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
-    PruebaGitHubTheme {
-        Greeting("Android")
+fun MainScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "Tarea de GitHub Actions",
+            style = MaterialTheme.typography.headlineMedium,
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(onClick = { /* Acción */ }) {
+            Text(text = "ENVIAR")
+        }
     }
 }
