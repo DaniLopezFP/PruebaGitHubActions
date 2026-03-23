c5693631f70590fc884478f18abecf653912ef7e
Daniel López Escuder <d.lopezescuder@edu.gva.es>
2026-03-23 13:30:11 +0100

    corrección 3 action Commit

--- app/src/main/java/com/example/pruebagithub/MainActivity.kt
+++ app/src/main/java/com/example/pruebagithub/MainActivity.kt
@@ -58,6 +58,9 @@ fun MainScreen() {
         Button(onClick = { /* Acción */ }) {
             Text(text = "ENVIAR")
         }
+        Button(onClick = { /* Acción */ }) {
+            Text(text = "REcibir")
+        }
 
     }
 }
